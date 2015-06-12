f6e7e93354668060c919a4ed7af1efaa16e04576
takuminnnn <skentaro36@gmail.com>
2015-06-12 15:03:16 +0900

    changed page links rails-style be more flexible in changing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
-
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root             'static_pages#home'
+  
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
