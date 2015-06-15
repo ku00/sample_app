0359168d79ea4552626724d31d87a0aead7d8882
takuminnnn <skentaro36@gmail.com>
2015-06-15 15:00:30 +0900

    improve email regex and added one invalid addresse in validation test of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
     presence: true,
     length: { maximum: 50}
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
    presence: true,
    length: { maximum: 255 },
