dd8235b35f73896122d6c5f8f50294061052902e
takuminnnn <skentaro36@gmail.com>
2015-06-16 14:14:01 +0900

    added two codes in user sign up test to solve exercise7.7-2

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,5 +23,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                 password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
   end
 end
