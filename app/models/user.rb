d890ca67e045cd04bcb3ceebfba5e2fb6f3699ee
takuminnnn <skentaro36@gmail.com>
2015-06-22 13:04:57 +0900

    added function to reset password finished 10.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   has_secure_password
 
   validates :name,
@@ -55,13 +55,28 @@ class User < ActiveRecord::Base
 
   # Activates an account.
   def activate
-    # update_attribute(:activated,    true)
     update_attributes({activated: true, activated_at: Time.zone.now})
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attributes({reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now})
   end
 
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
