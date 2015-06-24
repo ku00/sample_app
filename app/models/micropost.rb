a9b350d98a7480749d80337a9a18d42c0849fb8b
takuminnnn <skentaro36@gmail.com>
2015-06-24 12:15:14 +0900

    finished 11.4 not finish image upload in production (todo: write a separate PR)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,11 +1,22 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  mount_uploader :picture, PictureUploader
 
   validates :user_id,
     presence: true
   validates :content,
     presence: true,
     length: { maximum: 140 }
+  validate  :picture_size
 
   default_scope -> { order(created_at: :desc) }
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
