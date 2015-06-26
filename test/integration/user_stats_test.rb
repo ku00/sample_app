da46eafae79be6493baab51b2dfc771004eb708f
takuminnnn <skentaro36@gmail.com>
2015-06-26 17:56:48 +0900

    added tests for display user stats to solve exercise12.5-1

---
+++ test/integration/user_stats_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserStatsTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+
+  test "stas display" do
+    get user_path(@user)
+    assert_select "a[href=?]", following_user_path(@user)
+    assert_select "#following", text: "#{@user.following.count}"
+    assert_select "a[href=?]", followers_user_path(@user)
+    assert_select "#followers", text: "#{@user.followers.count}"
+  end
+end
