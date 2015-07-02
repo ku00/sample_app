c67b7acb032e64a280bfa3005056d3279040d286
takuminnnn <skentaro36@gmail.com>
2015-07-02 17:05:08 +0900

    reply to anyone by using post-form

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,13 @@ Rails.application.routes.draw do
       get :following, :followers, :notifications
     end
   end
+  resources :microposts, only: [:create, :destroy] do
+    member do
+      get :reply
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
